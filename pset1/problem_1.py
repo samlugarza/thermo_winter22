7d98165a6f0833e7964f2ef5195911abdf7895fb
Sam Garza <samgarza@uw.edu>
2022-01-14 09:29:59 -0800

    change demai

--- pset1/problem_1.py
+++ pset1/problem_1.py
@@ -3,7 +3,7 @@ from simulator import Simulation
 import matplotlib.pyplot as plt
 
 # create a new class for the simulation with some randomish variable choices
-sim = Simulation(N=100, E=10, size=750, rad=3, delay=200)
+sim = Simulation(N=100, E=10, size=750, rad=3, delay=50)
 
 # run the simulation
-sim.run_simulation(steps=10)
+sim.run_simulation(steps=1000)
